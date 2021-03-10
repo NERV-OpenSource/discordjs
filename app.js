57288c283fe28318b65d0f4012714c2abc34c808
Anderson Marlon <anderson18_marlon@hotmail.com>
2021-03-10 19:48:25 -0300

    initial commit

--- app.js
+++ app.js
@@ -1,39 +1,16 @@
-const Discord = require("discord.js");
-require('dotenv/config');
-const fs = require("fs");
-const path = require("path");
-
-
-
+const Discord = require('discord.js');
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
-client.queues = new Map();
 
-client.aliases = new Discord.Collection();
+require('dotenv/config');
 
-
-client.categories = fs.readdirSync('./commands/');
-
-['command'].forEach(handler => {
-    require(`./handlers/${handler}`)(client);
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-fs.readdir('./events/', (err, files) => {
-  if (err) return console.error;
-  files.forEach(file => {
-      if (!file.endsWith('.js')) return;
-      const evt = require(`./events/${file}`);
-      let evtName = file.split('.')[0];
-      console.log(`Evento carregado: '${evtName}'`);
-      client.on(evtName, evt.bind(null, client));
-  });
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
-
-
-client.on("ready", () => {
-  console.log(`Estou conectado como ${client.user.username}`);
-});
-
-
-client.login(process.env.TOKEN);
+client.login(process.env.API_TOKEN);
