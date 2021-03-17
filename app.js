ebb6e313932bcae696c9268d2e5f03801be83b7b
deivid procopio <Fukubi>
2021-03-17 16:15:37 -0300

    update: Starting to add roles by reaction

--- app.js
+++ app.js
@@ -14,90 +14,94 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-  const activeRole = message.guild.roles.cache.find(role => role.name = "Active");
-  if (message.author.bot) return;
+  try {
+    const activeRole = message.guild.roles.cache.find(role => role.name = "Active");
+    if (message.author.bot) return;
 
-  if (activeRole) {
-    if (!counter[message.author.id]) {
-      counter[message.author.id] = 1;
-    } else {
-      counter[message.author.id] += 1;
-    }
-
-    if (counter[message.author.id] >= 100 && !message.member.roles.cache.has(activeRole.id)) {
-      message.member.roles.add(activeRole).then((member) => {
-        message.reply("Parabéns, pela sua participação você ganhou um novo cargo!");
-      }).catch(console.error);
-    }
-  }
-
-  const prefix = process.env.PREFIX;
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-  const arguments = message.content.slice(prefix.length).trim().split(' ');
-  const command = arguments.shift().toLowerCase();
-
-  if (command === "ping") {
-    message.reply("Pong!");
-  }
-
-  if (command === "play") {
-    const voice = message.member.voice;
-    const URL = arguments[0];
-
-    if (!voice.channelID) {
-      message.reply("É preciso estar em um canal de voz para utilizar esse comando.");
-      return;
-    }
-
-    if (!URL) {
-      message.reply("É preciso enviar a URL do vídeo para ser reproduzido");
-      return;
-    }
-
-    voice.channel.join().then(async (connection) => {
-      try {
-        connection.play(await ytdl(arguments[0]), { type: 'opus' });
-      } catch (ex) {
-        message.reply("Erro ao reproduzir mídia");
+    if (activeRole) {
+      if (!counter[message.author.id]) {
+        counter[message.author.id] = 1;
+      } else {
+        counter[message.author.id] += 1;
       }
-    });
-  }
 
-  if (command === "stop") {
-    const voice = message.member.voice;
-
-    if (!voice.channelID) {
-      message.reply("É preciso estar em um canal de voz para utilizar esse comando.");
-      return;
+      if (counter[message.author.id] >= 100 && !message.member.roles.cache.has(activeRole.id)) {
+        message.member.roles.add(activeRole).then((member) => {
+          message.reply("Parabéns, pela sua participação você ganhou um novo cargo!");
+        }).catch(console.error);
+      }
     }
 
-    voice.channel.leave();
-  }
+    const prefix = process.env.PREFIX;
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  if (command === "roles") {
-    const getEmoji = emojiName => client.emojis.cache.find(emoji => emoji.name === emojiName);
+    const arguments = message.content.slice(prefix.length).trim().split(' ');
+    const command = arguments.shift().toLowerCase();
 
-    const emojis = {
-      ":thumbsup:": 'Thumbs Up',
-      ":thumbsdown:": 'Thumbs Down',
-    };
-
-    const reactions = [];
-
-    let emojiText = '';
-    for (const key in emojis) {
-      emoji = message.guild.emojis.cache.find(emoji => emoji.name === 'thumbsup');
-      reactions.push(emoji);
-
-      const role = emojis[key];
-      emojiText += `${emohu} = ${role}\n`
+    if (command === "ping") {
+      message.reply("Pong!");
     }
 
-    message.channel.send(emojiText).then((message) => {
-      message.react(message.guild.emojis.cache.find(emoji => emoji.name === 'thumbsup'));
-      message.react(message.guild.emojis.cache.find(emoji => emoji.name === "thumbsdown"));
-    });
+    if (command === "play") {
+      const voice = message.member.voice;
+      const URL = arguments[0];
+
+      if (!voice.channelID) {
+        message.reply("É preciso estar em um canal de voz para utilizar esse comando.");
+        return;
+      }
+
+      if (!URL) {
+        message.reply("É preciso enviar a URL do vídeo para ser reproduzido");
+        return;
+      }
+
+      voice.channel.join().then(async (connection) => {
+        try {
+          connection.play(await ytdl(arguments[0]), { type: 'opus' });
+        } catch (ex) {
+          message.reply("Erro ao reproduzir mídia");
+        }
+      });
+    }
+
+    if (command === "stop") {
+      const voice = message.member.voice;
+
+      if (!voice.channelID) {
+        message.reply("É preciso estar em um canal de voz para utilizar esse comando.");
+        return;
+      }
+
+      voice.channel.leave();
+    }
+
+    if (command === "roles") {
+      const getEmoji = emojiName => client.emojis.cache.find(emoji => emoji.name === emojiName);
+
+      const emojis = {
+        ":thumbsup:": 'Thumbs Up',
+        ":thumbsdown:": 'Thumbs Down',
+      };
+
+      const reactions = [];
+
+      let emojiText = '';
+      for (const key in emojis) {
+        emoji = message.guild.emojis.cache.find(emoji => emoji.name === 'thumbsup');
+        reactions.push(emoji);
+
+        const role = emojis[key];
+        emojiText += `${emoji} = ${role}\n`
+      }
+
+      message.channel.send(emojiText).then((message) => {
+        message.react(message.guild.emojis.cache.find(emoji => emoji.name === 'thumbsup'));
+        message.react(message.guild.emojis.cache.find(emoji => emoji.name === "thumbsdown"));
+      });
+    }
+  } catch (ex) {
+    message.reply("Ocorreu um erro interno, por favor relate isso aos moderadores.");
   }
 })
 
