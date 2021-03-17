57bb156efc91f944b58a26389c761c1d71ad3d0d
deivid procopio <Fukubi>
2021-03-17 18:33:54 -0300

    testing

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ client.on('message', message => {
           if (!user.bot && reaction.emoji.name === "👍") {
             message.channel.send("Handsup");
 
-            const role = message.guild.roles.cache.find((role) => role.name === "Github Team");
+            const role = message.guild.roles.cache.get("819220852913602590");
             console.log(role);
             const member = message.guild.members.cache.find((member) => member.id === user.id );
 
