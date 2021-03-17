e9590f02d3e8a4eb04e380163459f7a199bb7f32
deivid procopio <Fukubi>
2021-03-16 21:16:12 -0300

    fix: Returning everything to the app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 require('dotenv/config');
 const fs = require("fs");
-const path = require("path");
 
 function isAlreadyCounting(counter, memberId) {
   return counter[memberId] ? true : false;
@@ -29,26 +28,6 @@ client.commands = new Discord.Collection();
 client.queues = new Map();
 
 client.aliases = new Discord.Collection();
-
-
-client.categories = fs.readdirSync('./commands/');
-
-['command'].forEach(handler => {
-    require(`./handlers/${handler}`)(client);
-});
-
-fs.readdir('./events/', (err, files) => {
-  if (err) return console.error;
-  files.forEach(file => {
-      if (!file.endsWith('.js')) return;
-      const evt = require(`./events/${file}`);
-      let evtName = file.split('.')[0];
-      console.log(`Evento carregado: '${evtName}'`);
-      client.on(evtName, evt.bind(null, client));
-  });
-});
-
-
 
 client.on("ready", () => {
   console.log(`Estou conectado como ${client.user.username}`);
